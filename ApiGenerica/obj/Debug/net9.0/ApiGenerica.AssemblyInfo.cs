3b9e38d84aeaade658c7ab5c89c03dfc1a9a54e6
dano210905 <dany.hurtac@gmail.com>
2026-02-19 17:25:57 -0500

    Crear RepositorioLecturaMysqlMariaDB para MySQL y MariaDB

--- ApiGenerica/obj/Debug/net9.0/ApiGenerica.AssemblyInfo.cs
+++ ApiGenerica/obj/Debug/net9.0/ApiGenerica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGenerica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2b0ea896ec9c24d1a9b34434d7cf7fbd490b7e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63be33434aa0f9bdbb9b88831eafa4e784fcc6f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGenerica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGenerica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
