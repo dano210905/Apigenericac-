cf7265a4f269b53e68643a3d75d441b65b16189e
dano210905 <dany.hurtac@gmail.com>
2026-02-19 17:30:24 -0500

    Parte 12: Agregar EntidadesController - controlador genérico CRUD

--- ApiGenerica/obj/Debug/net9.0/ApiGenerica.AssemblyInfo.cs
+++ ApiGenerica/obj/Debug/net9.0/ApiGenerica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGenerica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b9e38d84aeaade658c7ab5c89c03dfc1a9a54e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4fbb5267744eb331a8bb1c589382d1e5c1c386")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGenerica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGenerica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
