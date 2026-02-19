e2b0ea896ec9c24d1a9b34434d7cf7fbd490b7e3
dano210905 <dany.hurtac@gmail.com>
2026-02-19 17:20:56 -0500

    Crear RepositorioLecturaSqlServer con detección automática de tipos

--- ApiGenerica/obj/Debug/net9.0/ApiGenerica.AssemblyInfo.cs
+++ ApiGenerica/obj/Debug/net9.0/ApiGenerica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGenerica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da92ab1bad4db3e9ccb9611b1915e34f769b152b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a8c1f4ccdc7bb10698c0925b575810517b2db19")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGenerica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGenerica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
